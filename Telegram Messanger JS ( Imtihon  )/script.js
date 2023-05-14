20df3e9b0937342d7bf732463c61078fa37ef3ac
Toxir <101031544+Toxir-Xakimov@users.noreply.github.com>
2023-05-14 14:06:08 +0500

    Add files via upload

---
+++ Telegram Messanger JS ( Imtihon  )/script.js
@@ -0,0 +1,268 @@
+let data = {
+    users: [
+        {
+            id: 1,
+            name: 'Steve',
+            bio: 'hello world !',
+            phone_number: +998333333333,
+            profile_picture: './img/FjU2lkcWYAgNG6d.jpg',
+            ownerMessage : [
+                {
+                    text: 'hello!',
+                    owner: true
+                },
+                {
+                    text: 'hi!',
+                    owner : false
+                }
+            ]
+        },
+
+        {
+            id: 2,
+            name: 'Zach',
+            bio: 'bye, bye world!',
+            phone_number: +998559018833,
+            profile_picture: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/9da965d4-9044-4a70-9d68-d7814d935b08/dfmqo97-9f9ee329-fa9c-4c09-8b86-d7db8e51d1f4.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzlkYTk2NWQ0LTkwNDQtNGE3MC05ZDY4LWQ3ODE0ZDkzNWIwOFwvZGZtcW85Ny05ZjllZTMyOS1mYTljLTRjMDktOGI4Ni1kN2RiOGU1MWQxZjQucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.gZcxgjUyjuoPWPbqFt9rfTDwOcb8lbapCbsiYEC30S8',
+            ownerMessage : [
+                {
+                    text: 'Qalesan ?',
+                    owner: true
+                },
+                {
+                    text:'zor man ',
+                    owner:false
+                }
+            
+            ]
+        },
+
+        {
+            id: 3,
+            name: 'Steven',
+            bio: 'Im Steven',
+            phone_number: +998958888888,
+            profile_picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUo4H-n4unWmeIFsQeSPxMZnWFX0Pk1ASmDw&usqp=CAU',
+            ownerMessage : [
+                {
+                    text: 'how are you ?',
+                    owner: true
+                },
+                {
+                    text: 'Im fine and you',
+                    owner: false
+                }
+             
+            ]
+        },
+
+        {
+            id: 4,
+            name: 'Chris',
+            bio: 'Im Chris',
+            phone_number: +998778937172,
+            profile_picture: 'https://upload.wikimedia.org/wikipedia/commons/e/e9/Albert-einstein-profile-picture-512x512.png.cf.png',
+            ownerMessage : [
+                {
+                    text: 'Qalesan ?',
+                    owner: true
+                },
+                {
+                    text: 'yaxshi',
+                    owner: false
+                }
+          
+            ]
+        },
+
+        {
+            id: 5,
+            name: 'Jhonson',
+            bio: 'Im from America!',
+            phone_number: +998333333333,
+            profile_picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRl94BUKaqlY-cvMMN9yJXviGTqRGQl6ca43ZBNkVysjg0donTKkuL6a_wBeSTnCpjUjUE&usqp=CAU',
+            ownerMessage : [
+                {
+                    text: 'Hi!',
+                    owner: true
+                },
+                {
+                    text: 'Hello',
+                    owner: false
+                }
+           
+            ]
+        },
+
+        {
+            id: 6,
+            name: 'Thompson',
+            bio: 'Im Thompson',
+            phone_number: +998333333333,
+            profile_picture: 'https://pm1.narvii.com/6573/fdf65fc486595d340b67a6002a15241dba16c72a_00.jpg',
+            ownerMessage : [
+                {
+                    text: 'Good Morning!',
+                    owner: true
+                },
+                {
+                    text:'Hi',
+                    owner:false
+                }
+            
+            ]
+        },
+
+        {
+            id: 7,
+            name: 'Carl',
+            bio: 'Im Carl!',
+            phone_number: +998333333333,
+            profile_picture: 'https://media.licdn.com/dms/image/D4D03AQHTTw8HHr9fmQ/profile-displayphoto-shrink_800_800/0/1680761678923?e=2147483647&v=beta&t=Lz9yBhrRRV1T3_bvr1mDrx8qGAUsIyJ1hKC6q4iOnG4',
+            ownerMessage : [
+                {
+                    text: 'Nice to meet you !',
+                    owner: true
+                },
+                {
+                    text:'Hi',
+                    owner:false
+                }
+             
+            ]
+        },
+
+        {
+            id: 8,
+            name: 'Alex',
+            bio: 'Im Alex!',
+            phone_number: +998888888888,
+            profile_picture: 'https://avatarfiles.alphacoders.com/946/94610.jpg',
+            ownerMessage : [
+                {
+                    text: 'Hi!!!!!',
+                    owner: true
+                },
+                {
+                    text:'Hi',
+                    owner:false
+                }
+          
+            ]
+        },
+
+        {
+            id: 9,
+            name: 'Simon',
+            bio: 'Im Simon!',
+            phone_number: +998101001010,
+            profile_picture: 'https://m.media-amazon.com/images/I/91BDAgAQiXL.png',
+            ownerMessage : [
+                {
+                    text: 'Good evening !',
+                    owner: true
+                },
+                {
+                    text:'Hi',
+                    owner:false
+                }
+             
+            ]
+        },
+
+        {
+            id: 10,
+            name: 'Mironsho',
+            bio: 'Man Mironsho!',
+            phone_number: +998999999999,
+            profile_picture: 'https://play-lh.googleusercontent.com/ip-IpVh7RsYCzA41cWVzpaT_iT068-tiyTg2k-KNeFqLpXWWBrNOhYrCKEnMQ_v4a4Q',
+            ownerMessage : [
+                {
+                    text: 'Qalesan!',
+                    owner: true
+                },
+                {
+                    text:'Hi',
+                    owner:false
+                }
+              
+            ]
+        },
+
+        {
+            id: 11,
+            name: 'Komil',
+            bio: 'Man Komilman',
+            phone_number: +998333333333,
+            profile_picture: 'https://www.shareicon.net/data/512x512/2016/07/26/802042_man_512x512.png',
+            ownerMessage : [
+                {
+                    text: 'Imtihonni qildingmi ?',
+                    owner: true
+                },
+                {
+                    text: 'Ha qildim',
+                    owner: false
+                }
+             
+            ]
+        }
+    ]
+}
+
+let messageList = document.querySelector('.messages')
+let Username = document.querySelector('.user-name')
+let Avatar = document.querySelector('.Profile-img')
+let contactList = document.querySelector('.contacts')
+
+let sendInput = document.querySelector('.send')
+let sendBtn = document.querySelector('.btn')
+
+sendBtn.onclick = function (event) {
+    event.preventDefault()
+    sendBtn = sendInput.value
+    console.log(sendBtn);
+}
+
+
+data.users.map((item, index) => {
+    let createElementLi = document.createElement('li')
+    let createElementImg = document.createElement('img')
+    createElementImg.setAttribute = 'src'
+    createElementImg.src = item.profile_picture
+    createElementLi.classList.add('contact-item')
+    createElementLi.textContent = item.name
+    contactList.appendChild(createElementLi)
+    console.log(createElementLi);
+    
+    createElementLi.addEventListener('click', () => {
+        messageList.textContent = '';
+        console.log(item.ownerMessage);
+        item.ownerMessage.forEach((message) => {
+            let newMessageElement = document.createElement('li')
+            newMessageElement.textContent = message.text
+            messageList.appendChild(newMessageElement)
+            
+            if(message.owner){
+                newMessageElement.classList.add('owner')
+               
+            } 
+    })
+
+    contactList.addEventListener('click', () => {
+        messageList.textContent = ''
+        Username.textContent = item.name;
+        Avatar.src = item.profile_picture
+        
+
+
+        } )
+    } )
+
+
+})
+
+Username.addEventListener('click',() => {
+    
+})
+
